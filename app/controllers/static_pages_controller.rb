1727aecbc5ac59530cae6e741e546f8d740b214d
Hurrendd <hurrendd.ramos@gmail.com>
2021-05-13 23:51:56 -0300

    Paginas Estaticas

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,12 @@ class StaticPagesController < ApplicationController
         @nome = "Hurrendd de Sousa Ramos"
         @nomefilha = "Lorenn Yorrana Rodrgues de Sousa"
     end
+
+    def sobre
+
+    end
+
+    def contato
+        
+    end
 end
